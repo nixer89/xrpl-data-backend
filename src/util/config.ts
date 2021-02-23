3da80a219e87cbdc67b9b4347f230386b26cc50f
nixer89 <pbnixer@gmail.com>
2021-02-23 16:22:34 +0100

    run on known, old port and change config

--- src/util/config.ts
+++ src/util/config.ts
@@ -1,2 +1,3 @@
-export const USE_PROXY = false;
-export const PROXY_URL = "http://proxy-pdb:81";
+export const USE_PROXY = process.env.USE_PROXY || false;
+export const PROXY_URL = process.env.PROXY_URL || "https://some-proxy.com:80"
+export const BITHOMP_TOKEN = process.env.BITHOMP_API_TOKEN;
