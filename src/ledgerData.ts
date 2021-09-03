301e72696a4d2a269d11531e6063f754673046ac
nixer89 <pbnixer@gmail.com>
2021-09-03 16:22:32 +0200

    v.1.7.0: collect lots of additional data!

--- src/ledgerData.ts
+++ src/ledgerData.ts
@@ -8,6 +8,16 @@ export class LedgerData {
     private ledgerData_1: any;
     private ledgerData_2: any;
 
+    FLAG_65536:number = 65536;
+    FLAG_131072:number = 131072;
+    FLAG_262144:number = 262144;
+    FLAG_524288:number = 524288;
+    FLAG_1048576:number = 1048576;
+    FLAG_2097152:number = 2097152;
+    FLAG_4194304:number = 4194304;
+    FLAG_8388608:number = 8388608;
+    FLAG_16777216:number = 16777216;
+
     private constructor() { }
 
     public static get Instance(): LedgerData
@@ -37,8 +47,7 @@ export class LedgerData {
               let newLedgerObject:any = {
                 count: 1,
                 size: size,
-                percentage: 0,
-                property_count: {}
+                percentage: 0
               }
 
               this.getLedgerData(load1)[ledgerObject.parsed.LedgerEntryType.toLowerCase()] = newLedgerObject;
@@ -94,11 +103,134 @@ export class LedgerData {
 
     addAdditionalProperty(load1: boolean, ledgerObject: any, property: string) {
       if(ledgerObject[property]) {
-        if(this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()]['property_count'][property])
-            this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()]['property_count'][property] = this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()]['property_count'][property] + 1;
-        else
-          this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()]['property_count'][property] = 1
+        this.increaseCountForProperty(load1, ledgerObject, "property_count", property, 1);
+
+        //special handling for some properties:
+        if("Balance" === property) {
+          if((ledgerObject[property].value && ledgerObject[property].value != "0") || (!ledgerObject[property].value && ledgerObject[property] != "0")) {
+            this.increaseCountForProperty(load1, ledgerObject, "special_data", "BalanceNotZero", 1);
+          }
+        }
+
+        if("OwnerCount" === property) {
+          //count not owner count!
+          this.increaseCountForProperty(load1, ledgerObject, "special_data", "OwnerCountTotal", ledgerObject[property]);
+        }
+
+        if("Amount" === property && !ledgerObject[property].value && !ledgerObject[property].issuer) {
+          //count total amount of XRP
+          this.increaseCountForProperty(load1, ledgerObject, "special_data", "AmountValueTotal", Number(ledgerObject[property]));
+        }
+
+        if("Balance" === property && !ledgerObject[property].value && !ledgerObject[property].issuer) {
+          //count total amount of XRP
+          this.increaseCountForProperty(load1, ledgerObject, "special_data", "BalanceValueTotal", Number(ledgerObject[property]));
+        }
+
+        if("SendMax" === property && !ledgerObject[property].value && !ledgerObject[property].issuer) {
+          //count total amount of XRP
+          this.increaseCountForProperty(load1, ledgerObject, "special_data", "SendMaxValueTotal", Number(ledgerObject[property]));
+        }
+
+        if("TakerGets" === property && !ledgerObject[property].value && !ledgerObject[property].issuer) {
+          //count total amount of XRP
+          this.increaseCountForProperty(load1, ledgerObject, "special_data", "XrpTotal", Number(ledgerObject[property]));
+        }
+
+        if("TakerPays" === property && !ledgerObject[property].value && !ledgerObject[property].issuer) {
+          //count total amount of XRP
+          this.increaseCountForProperty(load1, ledgerObject, "special_data", "XrpTotal", Number(ledgerObject[property]));
+        }
+
+        if("Flags" === property && ledgerObject[property]) {
+
+          if("account_root" === ledgerObject.LedgerEntryType.toLowerCase()) {
+            //Account Root Flags
+            if(this.isDefaultRippleEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfDefaultRipple", 1);
+
+            if(this.isDepositAuthEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfDepositAuth", 1);
+
+            if(this.isMasterKeyDisabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfDisableMaster", 1);
+
+            if(this.isDisallowXRPEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfDisallowXRP", 1);
+
+            if(this.isGlobalFreezeEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfGlobalFreeze", 1);
+
+            if(this.isNoFreezeEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfNoFreeze", 1);
+
+            if(this.isPasswordSpentEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfPasswordSpent", 1);
+
+            if(this.isRequireAuthEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfRequireAuth", 1);
+
+            if(this.isRequireDestinationTagEnabled(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfRequireDestTag", 1);
+          }
+
+          if("offer" === ledgerObject.LedgerEntryType.toLowerCase()) {
+            
+            //Offer Flags
+            if(this.isOfferFlagPassive(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfPassive", 1);
+
+            if(this.isOfferFlagSell(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfSell", 1);
+          }
+
+          if("signer_list" === ledgerObject.LedgerEntryType.toLowerCase()) {
+
+            //Signer List Flags
+            if(this.isSignerListFlagOneOwnerCount(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfOneOwnerCount", 1);
+          }
+
+          if("ripplestate" === ledgerObject.LedgerEntryType.toLowerCase()) {
+
+            if(this.isRippleStateFlagLowReserve(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfLowReserve", 1);
+            
+            if(this.isRippleStateFlagHighReserve(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfHighReserve", 1);
+
+            if(this.isRippleStateFlagLowAuth(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfLowAuth", 1);
+
+            if(this.isRippleStateFlagHighAuth(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfHighAuth", 1);
+
+            if(this.isRippleStateFlagLowNoRipple(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfLowNoRipple", 1);
+
+            if(this.isRippleStateFlagHighNoRipple(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfHighNoRipple", 1);
+
+            if(this.isRippleStateFlagLowFreeze(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfLowFreeze", 1);
+
+            if(this.isRippleStateFlagHighFreeze(ledgerObject[property]))
+              this.increaseCountForProperty(load1, ledgerObject, "flags", "lsfHighFreeze", 1);
+          }
+        }
       }
+    }
+
+    increaseCountForProperty(load1: boolean, ledgerObject: any, storageType:string, property: string, increaseBy: number) {
+      //create storage type if not set yet
+      if(!this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()][storageType])
+        this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()][storageType] = {}
+
+      //add property to storage and set a value
+      if(this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()][storageType][property])
+        this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()][storageType][property] = this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()][storageType][property] + increaseBy;
+      else
+        this.getLedgerData(load1)[ledgerObject.LedgerEntryType.toLowerCase()][storageType][property] = increaseBy;
     }
 
     public getLedgerData(load1: boolean) {
@@ -170,4 +302,84 @@ export class LedgerData {
         this.setLedgerData({}, load1);
       }  
     }
+
+  isDefaultRippleEnabled(flags:number) {
+      return flags && (flags & this.FLAG_8388608) == this.FLAG_8388608;
+  }
+
+  isDepositAuthEnabled(flags:number) {
+    return flags && (flags & this.FLAG_16777216) == this.FLAG_16777216;
+  }
+
+  isMasterKeyDisabled(flags:number) {
+    return flags && (flags & this.FLAG_1048576) == this.FLAG_1048576;
+  }
+
+  isDisallowXRPEnabled(flags:number) {
+    return flags && (flags & this.FLAG_524288) == this.FLAG_524288;
+  }
+
+  isGlobalFreezeEnabled(flags:number) {
+    return flags && (flags & this.FLAG_4194304) == this.FLAG_4194304;
+  }
+
+  isNoFreezeEnabled(flags:number) {
+    return flags && (flags & this.FLAG_2097152) == this.FLAG_2097152;
+  }
+  
+  isPasswordSpentEnabled(flags:number) {
+    return flags && (flags & this.FLAG_65536) == this.FLAG_65536;
+  }
+
+  isRequireAuthEnabled(flags:number) {
+    return flags && (flags & this.FLAG_262144) == this.FLAG_262144;
+  }
+
+  isRequireDestinationTagEnabled(flags:number) {
+      return flags && (flags & this.FLAG_131072) == this.FLAG_131072;
+  }  
+
+  isOfferFlagPassive(flags:number) {
+    return flags && (flags & this.FLAG_65536) == this.FLAG_65536;
+  }
+
+  isOfferFlagSell(flags:number) {
+    return flags && (flags & this.FLAG_131072) == this.FLAG_131072;
+  }
+
+  isSignerListFlagOneOwnerCount(flags:number) {
+    return flags && (flags & this.FLAG_65536) == this.FLAG_65536;
+  }
+
+  isRippleStateFlagLowReserve(flags:number) {
+    return flags && (flags & this.FLAG_65536) == this.FLAG_65536;
+  }
+
+  isRippleStateFlagHighReserve(flags:number) {
+    return flags && (flags & this.FLAG_131072) == this.FLAG_131072;
+  }
+
+  isRippleStateFlagLowAuth(flags:number) {
+    return flags && (flags & this.FLAG_262144) == this.FLAG_262144;
+  }
+
+  isRippleStateFlagHighAuth(flags:number) {
+    return flags && (flags & this.FLAG_524288) == this.FLAG_524288;
+  }
+
+  isRippleStateFlagLowNoRipple(flags:number) {
+    return flags && (flags & this.FLAG_1048576) == this.FLAG_1048576;
+  }
+
+  isRippleStateFlagHighNoRipple(flags:number) {
+    return flags && (flags & this.FLAG_2097152) == this.FLAG_2097152;
+  }
+
+  isRippleStateFlagLowFreeze(flags:number) {
+    return flags && (flags & this.FLAG_4194304) == this.FLAG_4194304;
+  }
+
+  isRippleStateFlagHighFreeze(flags:number) {
+    return flags && (flags & this.FLAG_8388608) == this.FLAG_8388608;
+  }
 }
