3b1b485ac0469c3fb62b4eda6dc3f62cb5cf43e3
nixer89 <pbnixer@gmail.com>
2021-10-05 22:30:34 +0200

    migration to xrpljs

--- src/ledgerScanner.ts
+++ src/ledgerScanner.ts
@@ -2,7 +2,7 @@ import * as scheduler from 'node-schedule';
 import consoleStamp = require("console-stamp");
 import { IssuerAccounts } from './issuerAccounts';
 import { LedgerData } from './ledgerData';
-import { Call, XrplClient } from 'xrpl-client';
+import { Client, LedgerDataRequest, LedgerDataResponse, LedgerRequest, LedgerResponse,  } from 'xrpl';
 
 consoleStamp(console, { pattern: 'yyyy-mm-dd HH:MM:ss' });
 
@@ -13,10 +13,12 @@ export class LedgerScanner {
     private load1: boolean = true;
     private isRunning:boolean = false;
 
-    private xrplClient:XrplClient;
+    //private xrplClient:XrplClient;
 
-    private ledger_index_1: string;
-    private ledger_index_2: string;
+    private xrpljsClient:Client;
+
+    private ledger_index_1: number;
+    private ledger_index_2: number;
     private ledger_date_1: string;
     private ledger_date_2: string;
     private ledger_time_ms_1: number;
@@ -26,6 +28,8 @@ export class LedgerScanner {
 
     private issuerAccount:IssuerAccounts;
     private ledgerData:LedgerData;
+
+    private index:number = 0;
 
     private constructor() {}
 
@@ -46,17 +50,17 @@ export class LedgerScanner {
         await this.issuerAccount.init(this.load1);
         await this.ledgerData.init(this.load1);
 
-        //await this.readLedgerData(null, null, null, 0);
+        await this.readLedgerData(null, null, null, 0);
 
         //load issuer data if it could not be read from the file system
-        if(this.load1 && this.issuerAccount.getIssuer_1().size == 0 || !this.load1 && this.issuerAccount.getIssuer_1().size == 0) {
-            await this.readLedgerData(null, null, null, 0);
-        }
+        //if(this.load1 && this.issuerAccount.getIssuer_1().size == 0 || !this.load1 && this.issuerAccount.getIssuer_1().size == 0) {
+        //    await this.readLedgerData(null, null, null, 0);
+        //}
 
         this.load1=!this.load1;
 
-        scheduler.scheduleJob("readIssuedToken", {minute: 0}, () => this.scheduleLoadingIssuerData());
-        scheduler.scheduleJob("readIssuedToken", {minute: 30}, () => this.scheduleLoadingIssuerData());
+        //scheduler.scheduleJob("readIssuedToken", {minute: 0}, () => this.scheduleLoadingIssuerData());
+        //scheduler.scheduleJob("readIssuedToken", {minute: 30}, () => this.scheduleLoadingIssuerData());
     }
 
     async scheduleLoadingIssuerData(): Promise<void> {
@@ -81,7 +85,7 @@ export class LedgerScanner {
       }
     }
 
-    public async readLedgerData(ledgerIndex:string, marker:string, oldMarker:string, retryCounter:number): Promise<boolean> {
+    public async readLedgerData(ledgerIndex:number, marker:unknown, oldMarker:unknown, retryCounter:number): Promise<boolean> {
         if(oldMarker && oldMarker == marker || (!marker && !oldMarker)) {
           console.log("increase retry counter");
           retryCounter++;
@@ -106,20 +110,20 @@ export class LedgerScanner {
             this.setLedgerHash(null);
         }
       
-      
-        let ledger_data_command_binary:Call = {
+        let ledger_data_command_binary:LedgerDataRequest = {
           command: "ledger_data",
           limit: 100000,
           binary: true
         }
+    
 
-        let ledger_data_command_json:Call = {
+        let ledger_data_command_json:LedgerDataRequest = {
           command: "ledger_data",
           limit: 100000,
           binary: false
         }
       
-        if(ledgerIndex)
+        if(ledgerIndex && typeof(ledgerIndex) === "number")
           ledger_data_command_binary.ledger_index = ledgerIndex;
         else
           ledger_data_command_binary.ledger_index = "validated";
@@ -128,12 +132,13 @@ export class LedgerScanner {
           ledger_data_command_binary.marker = ledger_data_command_json.marker = marker;
       
         try { 
-          if(!this.xrplClient || !this.xrplClient.getState().online) {
-              this.xrplClient = new XrplClient("ws://127.0.0.1:6006", {assumeOfflineAfterSeconds: 120});
+          if(!this.xrpljsClient || !this.xrpljsClient.isConnected()) {
+              this.xrpljsClient = new Client("ws://127.0.0.1:6006");
+              //this.xrpljsClient = new Client("wss://xrplcluster.com");
       
             try {
-              await this.xrplClient.ready();
-              console.log("connected to: " + JSON.stringify(this.xrplClient.getState().server));
+              await this.xrpljsClient.connect();
+              console.log("connected: " + JSON.stringify(this.xrpljsClient.isConnected()));
               //console.log(JSON.stringify(this.xrplClient.getState()));
               //console.log(JSON.stringify(await this.xrplClient.send({command: "", "__api":"state"})));
             } catch(err) {
@@ -145,17 +150,18 @@ export class LedgerScanner {
           //console.log("calling with: " + JSON.stringify(ledger_data_command));
           console.time("requesting binary");
           console.log("requesting with: " + JSON.stringify(ledger_data_command_binary))
-          let messageBinary = await this.xrplClient.send(ledger_data_command_binary);
-          console.log("length binary: " + messageBinary.state.length);
+          let messageBinary:LedgerDataResponse = await this.xrpljsClient.request(ledger_data_command_binary);
+          console.log("length binary: " + messageBinary.result.state.length);
           console.timeEnd("requesting binary");
                 
           //console.log("got response: " + JSON.stringify(message).substring(0,1000));
 
           //console.log(JSON.stringify(await this.xrplClient.send({command: "", "__api":"state"})));
       
-          if(messageBinary && messageBinary.state && messageBinary.ledger_index) {
-            let newledgerIndex:string = messageBinary.ledger_index;
-            let newMarker:string = messageBinary.marker;
+          if(messageBinary && messageBinary.result && messageBinary.result.state && messageBinary.result.ledger_index) {
+            let newledgerIndex:number = messageBinary.result.ledger_index;
+            console.log("marker: " + messageBinary.result.marker);
+            let newMarker:unknown = messageBinary.result.marker;
 
             console.log("newMarker: " + newMarker);
             //console.log("ledger_index: " + newledgerIndex);
@@ -164,35 +170,35 @@ export class LedgerScanner {
 
             console.time("requesting json");
             console.log("requesting with: " + JSON.stringify(ledger_data_command_json))
-            let messageJson = await this.xrplClient.send(ledger_data_command_json);
-            console.log("length json: " + messageJson.state.length);
+            let messageJson:LedgerDataResponse = await this.xrpljsClient.request(ledger_data_command_json);
+            console.log("length json: " + messageJson.result.state.length);
             console.timeEnd("requesting json");
 
-            if(messageJson && messageJson.state && messageJson.ledger_index == messageBinary.ledger_index && messageBinary.state.length == messageJson.state.length && messageBinary.marker == messageJson.marker) {
+            if(messageJson && messageJson.result && messageJson.result.state && messageJson.result.ledger_index == messageBinary.result.ledger_index && messageBinary.result.state.length == messageJson.result.state.length && messageBinary.result.marker == messageJson.result.marker) {
 
-              for(let i = 0; i < messageBinary.state.length; i++) {
+              for(let i = 0; i < messageBinary.result.state.length; i++) {
                 try {
-                  if(messageBinary.state[i].index == messageJson.state[i].index)
-                    messageBinary.state[i].parsed = messageJson.state[i];
+                  if(messageBinary.result.state[i].index == messageJson.result.state[i].index)
+                    messageBinary.result.state[i]['parsed'] = messageJson.result.state[i];
                   else {
                     console.log("####### NOT SAME INDEX!!! ###########")
-                    console.log("BINARY: " + messageBinary.state[i].index);
-                    console.log("JSON  : "  + messageJson.state[i].index)
+                    console.log("BINARY: " + messageBinary.result.state[i].index);
+                    console.log("JSON  : "  + messageJson.result.state[i].index)
                   }
                 } catch(err) {
                   console.log(err);
-                  console.log("binary: " + JSON.stringify(messageBinary.state[i]));
-                  console.log("json: " + JSON.stringify(messageJson.state[i]));
+                  console.log("binary: " + JSON.stringify(messageBinary.result.state[i]));
+                  console.log("json: " + JSON.stringify(messageJson.result.state[i]));
                   return;
                 }
               }
 
               console.time("resolveLedgerData binary");
-              await this.ledgerData.resolveLedgerData(messageBinary.state, this.load1);
+              await this.ledgerData.resolveLedgerData(messageBinary.result.state, this.load1);
               console.timeEnd("resolveLedgerData binary");
               
               console.time("resolveIssuerToken");
-              await this.issuerAccount.resolveIssuerToken(messageJson.state, this.load1);
+              await this.issuerAccount.resolveIssuerToken(messageJson.result.state, this.load1);
               console.timeEnd("resolveIssuerToken");
             } else {
               throw "binary and json objects not the same!"
@@ -201,9 +207,10 @@ export class LedgerScanner {
       
             console.log("issuer_1 size: " + this.issuerAccount.getIssuer_1().size);
             console.log("issuer_2 size: " + this.issuerAccount.getIssuer_2().size);
-            if(newledgerIndex != null && newMarker != null)
+            if(newledgerIndex != null && newMarker != null && this.index < 10) {
+              this.index++;
                 return this.readLedgerData(newledgerIndex, newMarker, marker, retryCounter);
-            else {
+            } else {
               console.log("Done 1");
               console.log("issuer_1 size: " + this.issuerAccount.getIssuer_1().size);
               console.log("issuer_2 size: " + this.issuerAccount.getIssuer_2().size);
@@ -215,13 +222,18 @@ export class LedgerScanner {
           }
       
           console.log("ALL DONE");
+
+          let ledgerCommand:LedgerRequest = {
+            command: "ledger",
+            ledger_index: ledgerIndex
+          }
                 
-          let ledgerInfo = await this.xrplClient.send({command: "ledger", ledger_index: ledgerIndex});
+          let ledgerInfo:LedgerResponse = await this.xrpljsClient.request(ledgerCommand);
       
           this.setLedgerIndex(ledgerIndex);
-          this.setLedgerHash(ledgerInfo.ledger_hash);
-          this.setLedgerCloseTime(ledgerInfo.ledger.close_time_human);
-          this.setLedgerCloseTimeMs(ledgerInfo.ledger.close_time);
+          this.setLedgerHash(ledgerInfo.result.ledger_hash);
+          this.setLedgerCloseTime(ledgerInfo.result.ledger.close_time_human);
+          this.setLedgerCloseTimeMs(ledgerInfo.result.ledger.close_time);
 
           //always save resolved user names to file system to make restart of server much faster
           setTimeout(() => this.issuerAccount.saveBithompNamesToFS(), 10000);
@@ -229,12 +241,7 @@ export class LedgerScanner {
           await this.ledgerData.saveLedgerDataToFS(this.load1);
 
           //trigger online deletion
-          let onlineDeletionCommand:Call = {
-            command: "can_delete",
-            can_delete: "now"
-          }
-
-          await this.xrplClient.send(onlineDeletionCommand);
+          //await this.xrpljsClient.request({command: "can_delete", can_delete: "now"});
     
           return true;
       
@@ -248,15 +255,15 @@ export class LedgerScanner {
         }
       }
     
-      public getLedgerIndex(): string {
+      public getLedgerIndex(): number {
         return this.load1 ? this.ledger_index_2 : this.ledger_index_1;
     }
 
-    public getLedgerIndexNew(): string {
+    public getLedgerIndexNew(): number {
       return this.load1 ? this.ledger_index_1 : this.ledger_index_2;
     }
 
-    public setLedgerIndex(index:string): void {
+    public setLedgerIndex(index:number): void {
       if(this.load1)
         this.ledger_index_1 = index;
       else
