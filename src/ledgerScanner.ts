ca4007266bc4f928587b5014899c5b15199c388d
nixer89 <pbnixer@gmail.com>
2022-07-18 12:52:19 +0200

    set to 75k

--- src/ledgerScanner.ts
+++ src/ledgerScanner.ts
@@ -51,7 +51,7 @@ export class LedgerScanner {
         //read first ledger!
         //await this.readLedgerData(32570, null, null, 0);
 
-        for(let i = 50000; i < 73051000; i = i + 100000) {
+        for(let i = 75000; i < 73080000; i = i + 100000) {
           await this.readLedgerData(i, null, null, 0);
         }
 
