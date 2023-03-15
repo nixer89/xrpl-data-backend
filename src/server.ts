0fc3fff67a5fd608e891ee9a776e1554a00ddcb3
nixer89 <pbnixer@gmail.com>
2023-03-15 10:44:05 +0100

    v.3.6.1: create folder if not existent

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import consoleStamp = require("console-stamp");
 import { LedgerScanner } from './ledgerScanner';
 import { SelfAssessments } from "./selfAssessments";
+import * as fs from 'fs';
+import { DATA_PATH } from "./util/config";
 
 let ledgerScanner:LedgerScanner = LedgerScanner.Instance;
 let selfAssessments:SelfAssessments = SelfAssessments.Instance;
@@ -10,6 +12,10 @@ consoleStamp(console, { pattern: 'yyyy-mm-dd HH:MM:ss' });
 // Run the server!
 const start = async () => {
   setTimeout(() => {
+
+    if(!fs.existsSync(DATA_PATH))
+      fs.mkdirSync(DATA_PATH);
+
     selfAssessments.init();
     ledgerScanner.init();
   },0);
