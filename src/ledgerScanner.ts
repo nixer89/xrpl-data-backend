d4dabfec3358b96f0ba72e2b5a7e5640b23f566e
nixer89 <pbnixer@gmail.com>
2023-10-11 07:47:54 +0200

    t

--- src/ledgerScanner.ts
+++ src/ledgerScanner.ts
@@ -165,14 +165,14 @@ export class LedgerScanner {
       
         let ledger_data_command_binary:LedgerDataRequest = {
           command: "ledger_data",
-          limit: this.runs < 32 ? 100000 : 50000,
+          limit: 50000,
           binary: true
         }
     
 
         let ledger_data_command_json:LedgerDataRequest = {
           command: "ledger_data",
-          limit: this.runs < 32 ? 100000 : 50000,
+          limit: 50000,
           binary: false
         }
       
